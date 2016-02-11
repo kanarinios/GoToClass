706e49223d27cbc881dfc47e1e0aca7aa4722741
Dariusz Bliszczyk <dariusz.bliszczyk@gmail.com>
2016-02-11 11:38:37 +0100

    Add go_to_class_word_separators settings for Base File.sublime-settings. Add Default.sublime-mousemap with mouse kayboard shourtcut. Update GoToClass to use go_to_class_word_separators instead of Default Sublime word_separators. Add workaround for matching/highlighting first match in go_to anywhere.

--- GoToClass.py
+++ GoToClass.py
@@ -7,6 +7,10 @@ class GoToClassCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         settings = self.view.settings()
         separator = settings.get("go_to_class_separator")
+        old_word_separators = settings.get("word_separators")
+        go_to_class_word_separators = settings.get("go_to_class_word_separators")
+
+        settings.set("word_separators", go_to_class_word_separators)
 
         for sel in self.view.sel():
             if sel.empty():
@@ -14,6 +18,11 @@ class GoToClassCommand(sublime_plugin.TextCommand):
             word_sel = self.view.substr(sel)
             word_sel = word_sel.replace(separator, ' ')
             self.view.window().run_command("show_overlay", {"overlay": "goto", "text": word_sel})
+            ## Workaraound for selecting first match in GoTo Anywhere:
+            self.view.window().run_command("indent")
+            self.view.window().run_command("unindent")
+
+        settings.set("word_separators", old_word_separators)
 
 
 class GoToParentClassCommand(sublime_plugin.TextCommand):
